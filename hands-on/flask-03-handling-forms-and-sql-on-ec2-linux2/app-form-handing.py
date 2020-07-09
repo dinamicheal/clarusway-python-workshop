e2fe595f5568ad5a16a6fe5eb2543319f34ba20d
dodo <dinamicheal82@gmail.com>
2020-07-09 18:54:50 -0400

    new

--- hands-on/flask-03-handling-forms-and-sql-on-ec2-linux2/app-form-handing.py
+++ hands-on/flask-03-handling-forms-and-sql-on-ec2-linux2/app-form-handing.py
@@ -18,5 +18,5 @@ def login():
     else:
         return render_template('login.html')    
 if __name__ == '__main__':
-    app.run(debug=True)
-    #app.run('0.0.0.0', port=80)
+    #app.run(debug=True)
+    app.run('0.0.0.0', port=80)
